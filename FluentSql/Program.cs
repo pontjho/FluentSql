eca774c99389b351c192d58f043728a1ed3d64df
Pontjho Mokhonoana <pontjho@gmail.com>
2013-02-11 21:12:18 +0200

    Basic test and implementation

---
+++ FluentSql/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace FluentSql
+{
+    public static class Fluent
+    {
+        private static String config = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+        public static Statement Default = new Statement(config);
+
+        internal static void ExecuteSqlCode(string sqlCommandText, SqlConnection conn)
+        {
+            var cmd = new SqlCommand(sqlCommandText, conn);
+            cmd.ExecuteNonQuery();
+        }
+        public static NonQueryStatement WithNonQueryStatement(this NonQueryStatement statement, String newStatement)
+        {
+            var newQueries = statement.Actions.Union(new String[] { newStatement });
+            return new NonQueryStatement(statement.ConnectionString, newQueries);
+        }
+
+        public static ScalarQueryStatement<T> WithScalarQueryStatement<T>(this NonQueryStatement statement, String newQueryStatement)
+        {
+            return new ScalarQueryStatement<T>(statement.ConnectionString, statement.Actions, newQueryStatement);
+        }
+
+        public static ReaderQueryStatement<T> WithReaderQueryStatement<T>(this NonQueryStatement statement, String newQueryStatement, Func<SqlDataReader, T> readerFunction)
+        {
+            return new ReaderQueryStatement<T>(statement.ConnectionString, statement.Actions, newQueryStatement, readerFunction);
+        }
+
+        /*          */
+        public static NonQueryStatement WithNonQueryStatement(this Statement statement, String newStatement)
+        {
+            var newQueries = new String[] { newStatement };
+            return new NonQueryStatement(statement.ConnectionString, newQueries);
+        }
+
+        public static ScalarQueryStatement<T> WithScalarQueryStatement<T>(this Statement statement, String newQueryStatement)
+        {
+            var newQueries = new String[] { newQueryStatement };
+            return new ScalarQueryStatement<T>(statement.ConnectionString, newQueries, newQueryStatement);
+        }
+
+        public static ReaderQueryStatement<T> WithReaderQueryStatement<T>(this Statement statement, String newQueryStatement, Func<SqlDataReader, T> readerFunction)
+        {
+            var newQueries = new String[] { newQueryStatement };
+            return new ReaderQueryStatement<T>(statement.ConnectionString, newQueries, newQueryStatement, readerFunction);
+        }
+    }
+
+    public class Statement
+    {
+        internal String ConnectionString { get; private set; }
+
+        public Statement(String connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+    }
+
+    public static class StatementExtensions
+    {
+    }
+
+    public class NonQueryStatement : Statement
+    {
+        internal IEnumerable<String> Actions { get; private set; }
+
+        internal NonQueryStatement(String connectionString, IEnumerable<String> actions)
+            : base(connectionString)
+        {
+            this.Actions = new List<String>(actions);
+        }
+
+        public void Execute()
+        {
+            using (var cn = new SqlConnection(this.ConnectionString))
+            {
+                cn.Open();
+                Actions.ToList().ForEach(actionText => Fluent.ExecuteSqlCode(actionText, cn));
+            }
+        }
+    }
+
+    public class ScalarQueryStatement<T>
+    {
+        private List<String> NonQueryActions { get; set; }
+        private String ConnectionString { get; set; }
+        private String ScalarAction { get; set; }
+
+        internal ScalarQueryStatement(String connectionString, IEnumerable<String> nonQueryActions, String scalarAction)
+        {
+            this.NonQueryActions = new List<String>(nonQueryActions);
+            this.ConnectionString = connectionString;
+            this.ScalarAction = scalarAction;
+        }
+
+        public T Execute()
+        {
+            using (var cn = new SqlConnection(this.ConnectionString))
+            {
+                cn.Open();
+                NonQueryActions.ToList().ForEach(actionText => Fluent.ExecuteSqlCode(actionText, cn));
+
+                var cmd = new SqlCommand(this.ScalarAction, cn);
+                try
+                {
+                    return (T)(Object)Int32.Parse(cmd.ExecuteScalar().ToString());
+                }
+                catch (InvalidCastException)
+                {
+                    return (T)cmd.ExecuteScalar();
+                }
+            }
+        }
+    }
+
+    public class ReaderQueryStatement<T>
+    {
+        private List<String> NonQueryActions { get; set; }
+        private String ConnectionString { get; set; }
+        private String ScalarAction { get; set; }
+        private Func<SqlDataReader, T> ParseLine { get; set; }
+
+        internal ReaderQueryStatement(String connectionString, IEnumerable<String> nonQueryActions, String scalarAction, Func<SqlDataReader, T> lineReader)
+        {
+            this.NonQueryActions = new List<String>(nonQueryActions);
+            this.ConnectionString = connectionString;
+            this.ScalarAction = scalarAction;
+            this.ParseLine = lineReader;
+        }
+
+        public IEnumerable<T> Execute()
+        {
+            using (var cn = new SqlConnection(this.ConnectionString))
+            {
+                cn.Open();
+                NonQueryActions.ToList().ForEach(actionText => Fluent.ExecuteSqlCode(actionText, cn));
+
+                var cmd = new SqlCommand(this.ScalarAction, cn);
+
+                var resultSet = cmd.ExecuteReader();
+
+                var theReturn = new List<T>();
+                while (resultSet.Read())
+                {
+                    theReturn.Add(ParseLine(resultSet));
+                }
+                return theReturn;
+            }
+        }
+    }
+}
---
+++ LibTest/IntegrationTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentSql;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Linq;
+
+namespace LibTest
+{
+    [TestClass]
+    public class IntegrationTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            Fluent.Default.WithNonQueryStatement("if exists(select * from sys.databases where name = 'Test') drop database Test").Execute();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Fluent.Default.WithNonQueryStatement("if exists(select * from sys.databases where name = 'Test') drop database Test").Execute();
+        }
+
+        [TestMethod]
+        public void GivenAQueryToCreateADbWithASingleTable_WhenExecutingTheQuery_TheMasterDbWillContainTheTable()
+        {
+            var query = Fluent.Default.WithNonQueryStatement("create database Test")
+                                      .WithNonQueryStatement("use Test")
+                                      .WithNonQueryStatement("create table TestTable (a int, b int)");
+
+            query.Execute();
+
+            Assert.AreEqual<String>("TestTable", GetTable());
+        }
+
+        [TestMethod]
+        public void GivenAQueryToGetTheTablesFromTheDatabase_WhenExecutingTheQueryAsScalar_TheValueIsReturned()
+        {
+            var query = Fluent.Default.WithNonQueryStatement("create database Test")
+                                      .WithNonQueryStatement("use Test")
+                                      .WithNonQueryStatement("create table TestTable (a int, b int)")
+                                      .WithNonQueryStatement("insert into TestTable (a , b) values (5, 6)")
+                                      .WithScalarQueryStatement<int>("select a from TestTable");
+
+            var result = query.Execute();
+
+            Assert.AreEqual<int>(5, result);
+        }
+
+        [TestMethod]
+        public void GivenAQueryToGetTheTablesFromTheDatabase_WhenExecutingTheQueryAsResultSet_TheValuesAreReturned()
+        {
+            var query = Fluent.Default.WithNonQueryStatement("create database Test")
+                                      .WithNonQueryStatement("use Test")
+                                      .WithNonQueryStatement("create table TestTable (a int, b varchar(10))")
+                                      .WithNonQueryStatement("insert into TestTable (a , b) values (5, 'hello')")
+                                      .WithNonQueryStatement("insert into TestTable (a , b) values (7, 'world')")
+                                      .WithReaderQueryStatement("select a, b from TestTable", TheReader);
+
+            var result = query.Execute().ToList();
+
+            var expected = new Object[]{ Tuple.Create(5, "hello"), Tuple.Create(7, "world")};
+
+            Assert.AreEqual(expected[0], result[0]);
+            Assert.AreEqual(expected[1], result[1]);
+        }
+
+        public Tuple<int, String> TheReader(SqlDataReader reader)
+        {
+            return Tuple.Create(reader.GetInt32(0), reader.GetString(1));
+        }
+
+        public String GetTable()
+        {
+            using (var cn = new SqlConnection(ConfigurationManager.ConnectionStrings["Default"].ConnectionString))
+            {
+                cn.Open();
+                var cmd = new SqlCommand("SELECT TABLE_NAME FROM Test.information_schema.tables", cn);
+                return cmd.ExecuteScalar().ToString();
+            }
+        }
+    }
+}
