d79a108cfd93f534850c3b39c2daaa1e35a4f546
Pontjho Mokhonoana <pontjho@gmail.com>
2013-02-23 23:10:35 +0200

    New file

---
+++ FluentSql/ExecutableStatement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentSql
+{
+    public class ExecutableStatement : Statement
+    {
+        internal IEnumerable<NonQueryAction> NonQueryActions { get; private set; }
+
+        public ExecutableStatement(String connectionString, IEnumerable<NonQueryAction> actions) : base(connectionString)
+        {
+            this.NonQueryActions = actions;
+        }
+    }
+}
